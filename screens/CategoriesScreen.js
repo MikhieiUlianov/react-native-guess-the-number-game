ed78ad6c1612a42329fd3c7361b2c22797866f4e
Mkh <example@gmail.com>
2025-09-26 19:05:03 +0200

    displaying-grids

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+const styles = StyleSheet.create({
+  root: {},
+});
+export default CategoriesScreen;
